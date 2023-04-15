a06760a323b30b01e5136b5a764d806028333325
Ross Condie <rosscondie@gmail.com>
2023-04-15 14:49:46 +0200

    cabin crew member class and tests - all passing

---
+++ src/main/java/CabinCrewMember.java
@@ -0,0 +1,18 @@
+public class CabinCrewMember {
+
+    private String name;
+    private String rank;
+
+    public CabinCrewMember(String name, String rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+}
---
+++ src/test/java/CabinCrewMemberTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CabinCrewMemberTest {
+
+    CabinCrewMember cabinCrewMember;
+
+    @Before
+    public void before() {
+        cabinCrewMember = new CabinCrewMember("Sheila Skye", "Flight Attendant");
+    }
+
+    @Test
+    public void cabinCrewMemberHasName() {
+        assertEquals("Sheila Skye", cabinCrewMember.getName());
+    }
+
+    @Test
+    public void cabinCrewMemberHasRank() {
+        assertEquals("Flight Attendant", cabinCrewMember.getRank());
+    }
+}
